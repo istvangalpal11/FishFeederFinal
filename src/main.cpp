ef0ea57b406a1773445f9010a6f3089f275a0e1e
istvangalpal11 <istvangalpal11@gmail.com>
2025-07-30 11:16:22 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myServo;
+
+// test servo angle sweep
+
+void setup() {
+  myServo.attach(9);  // Control pin
+}
+
+void loop() {
+  myServo.write(0);     // Turn to 0°
+  delay(1000);
+  myServo.write(90);    // Turn to 90°
+  delay(1000);
+  myServo.write(180);   // Turn to 180°
+  delay(1000);
+}
